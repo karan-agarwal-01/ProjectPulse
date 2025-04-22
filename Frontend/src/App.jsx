8cb9132671c4129b5a10294f7efc719aa8032f3a
karan-agarwal-01 <karanagarwal858@gmail.com>
2025-04-22 13:31:29 +0530

    new changes added

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,21 +1,23 @@
 import './App.css';
 import Home from './components/Main/Home';
 import Navbar from './components/Navbar/Navbar';
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import {Route, Routes, useLocation} from 'react-router-dom';
 import Dashboard from './components/Workflow/Admin/Dashboard';
 import Project_Management from './components/Workflow/Admin/Project_Management';
+import AdminLogin from './components/Auth/Login';
 
 function App() {
 
+  const location = useLocation();
+
   return (
     <>
-    <Navbar />
-    <BrowserRouter>
+     {location.pathname !== '/login' && <Navbar />}
       <Routes>
         <Route path='/' element={<Home render={<Dashboard />} />} />
         <Route path='/manage-projects' element={<Home render={<Project_Management />} />} />
+        <Route path='/login' element={<AdminLogin />} />
       </Routes>
-    </BrowserRouter>
     </>
   )
 }
--- Frontend/src/components/Auth/Login.jsx
+++ Frontend/src/components/Auth/Login.jsx
@@ -1,14 +1,8 @@
 const AdminLogin = () => {
 
   return (
-    <div className="w-screen h-screen bg-[#f0f0f0] flex justify-between relative overflow-hidden">
-      <div className="min-w-[1200px] min-h-[1200px]  bg-[#3e11aa] rotate-[-60deg] absolute -left-120 -top-100">
-        <div className="min-w-[400px] min-h-[400px]  rotate-[60deg] text-white text-center absolute bottom-50 right-90">
-          <h1 className="text-8xl">Welcome</h1>
-          <h3 className="text-right text-2xl">ProjectPulse</h3>
-        </div>
-      </div>
-      <div className="min-w-[500px] min-h-auto border-2 border-[#1b1a1a46]  text-[#1b1a1a6a] absolute top-70 -right-10 -translate-30 text-center p-5 rounded-xl flex flex-col justify-around">
+    <div className="w-screen h-screen relative gardient overflow-hidden">
+      <div className="min-w-[400px] min-h-auto border-2 border-[#1b1a1a46]  text-[#1b1a1a6a] absolute top-[50%] -right-[10%] -translate-[50%] text-center p-5 rounded-xl flex flex-col justify-around">
         <h2 className="text-2xl pb-3">Admin Login</h2>
         <div className="w-auto h-[300px] flex flex-col justify-around">
           <input className="p-3 border-b-2 border-[#1b1a1a46] focus:outline-none"
